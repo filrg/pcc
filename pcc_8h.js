97f6b305e72a37bb729117ceaf7549242e76d64a
QuangNguyenLong <QuangNguyenLong@users.noreply.github.com>
2025-05-16 03:52:31 +0000

    deploy: 93b7ca02a6248c17e109bdc532e663bdf05fb1b2

--- pcc_8h.js
+++ pcc_8h.js
@@ -1,5 +1,7 @@
 var pcc_8h =
 [
-    [ "pcc_decode", "pcc_8h.html#afe32a66f2a65fe9ef311b1516124661d", null ],
-    [ "pcc_encode", "pcc_8h.html#a9ef5958322c2b9eebf2f98878878eccf", null ]
+    [ "pcc_decode", "pcc_8h.html#a2b1d9ff64e10ae84dad8eafb2e35bae1", null ],
+    [ "pcc_encode", "pcc_8h.html#ab1faa229e0deb9b84cfefb3a91a19185", null ],
+    [ "pcc_multiple_decode", "pcc_8h.html#ac5d149d68a03ae3b499602fd80bc2459", null ],
+    [ "pcc_multiple_encode", "pcc_8h.html#a70ebe4d772bc0f2095154b8a61a99685", null ]
 ];
